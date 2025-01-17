31d1718b266f0dd2f80247b2b01a7c4a0b7fe131
Gleb Fedchun <gleb.fedchun@gmail.com>
2025-01-17 06:10:43 +0300

    fimna

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from'react';
+import axios from 'axios';
+
+import './App.css';
+function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('https://wbcraper.onrender.com/wb/parse');
+      const data = response.data;
+      const productsList = [];
+
+      data.forEach(item => {
+        item.Ret.forEach(product => {
+          productsList.push({
+            id: product.Id,
+            name: product.Name,
+            points: product.Points,
+            price: product.Price,
+            url: product.Url,
+            urlImg: product.UrlImg,
+          });
+        });
+      });
+
+      setProducts(productsList);
+    } catch (error) {
+      console.error('Ошибка:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Список товаров</h1>
+      <button onClick={fetchProducts}>Загрузить товары</button>
+      {loading? (
+        <p>Загрузка...</p>
+      ) : (
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          {products.map((product) => (
+            <div key={product.id} className="product-card">
+              <h2>{product.name}</h2>
+              <p>Цена:{product.price}</p>
+              <p>Баллы:{product.points}</p>
+              <img src={product.urlImg} alt={product.name} />
+              <a href={product.url} target="_blank" rel="noopener noreferrer">
+                <button className="btn">Перейти к товару</button>
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+  
+}
+
+export default App;
